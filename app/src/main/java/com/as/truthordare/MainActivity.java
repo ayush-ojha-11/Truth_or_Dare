f62568fbbefc79f10e05567563105426e51a081a
ayush-ojha-11 <ayushojha1512@gmail.com>
2023-04-23 11:16:51 +0530

    2nd commit

--- app/src/main/java/com/as/truthordare/MainActivity.java
+++ app/src/main/java/com/as/truthordare/MainActivity.java
@@ -1,8 +1,12 @@
 package com.as.truthordare;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
@@ -11,20 +15,28 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    ImageView arrow;
+    ImageView arrow,openMenuButton;
     int currentPoint,nextPoint;
 
     TextView headingTV;
     LinearLayout buttonHolder;
 
     Button truth,dare;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +49,9 @@ public class MainActivity extends AppCompatActivity {
         buttonHolder = findViewById(R.id.buttonHolderLayout);
         truth = findViewById(R.id.truthButton);
         dare = findViewById(R.id.dareButton);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.nav_View);
+        openMenuButton = findViewById(R.id.openMenuIV);
 
         arrow.setOnClickListener(v -> spinArrow());
 
@@ -51,6 +66,17 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("flag","2");
             startActivity(intent);
         });
+
+
+        openMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+
+
+        navigationView.setNavigationItemSelectedListener(MainActivity.this);
 
 
     }
@@ -95,5 +121,40 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+            if(id==R.id.menu_about) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setIcon(R.drawable.baseline_info_24);
+                builder.setTitle("About");
+                builder.setMessage(R.string.about);
+                builder.setPositiveButton("Ok", (dialog, which) -> dialog.cancel());
+                builder.show();
+            }
+
+            else if(id==R.id.menu_share){
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT,"Found an amazing game!\n"+"https://play.google.com/store/apps/details?id=com.as.truthordare");
+                sendIntent.setType("text/plain");
+                sendIntent.putExtra(Intent.EXTRA_TITLE,"Sharing the App!");
+                Intent shareIntent = Intent.createChooser(sendIntent,null);
+                startActivity(shareIntent);
+            }
+            else if(id==R.id.menu_email){
+                String email ="ayushojha1512@gmail.com";
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"+email));
+                startActivity(Intent.createChooser(emailIntent,"Mail"));
+            }
+            else if (id==R.id.menu_exit){
+                finishAffinity();
+            }
+
+        return false;
     }
 }
