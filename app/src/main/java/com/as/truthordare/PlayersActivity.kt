631782f9f8dfd131a91965056f9d3b1d35399cd0
ayush-ojha-11 <ayushojha1512@gmail.com>
2023-05-26 20:07:41 +0530

    New Commit after Kotlin integration

---
+++ app/src/main/java/com/as/truthordare/PlayersActivity.kt
@@ -0,0 +1,11 @@
+package com.`as`.truthordare
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlayersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_players)
+    }
+}
